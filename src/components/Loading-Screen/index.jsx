55ebcff81b5060da6ba8e5f02420551d29bfc877
yasirulakshan <yasiru1999@gmail.com>
2024-07-18 00:50:11 +0530

    update pace import

--- src/components/Loading-Screen/index.jsx
+++ src/components/Loading-Screen/index.jsx
@@ -25,7 +25,7 @@ const LoadingScreen = () => {
         <Script
           id="pace"
           strategy="beforeInteractive"
-          src="/js/pace.min.js"
+          src="./js/pace.min.js"
         ></Script>
       ) : (
         ""
