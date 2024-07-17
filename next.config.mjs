db577354612670a7e9ca2f221e1796c7bd13ca15
yasirulakshan <yasiru1999@gmail.com>
2024-07-17 23:53:40 +0530

    refactor: Update next.config.mjs to include output and basePath properties
    This commit updates the next.config.mjs file to include the output property set to "export" and the basePath property set to "/my-portfolio". These changes ensure that the Next.js configuration is correctly set up for exporting the project and setting the base path for the repository.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/my-portfolio", // Adjust according to your repository name
 };
-
 export default nextConfig;
