928f74c0855cd8e01853db28f47b990d08052dca
yasirulakshan <yasiru1999@gmail.com>
2024-07-18 00:32:35 +0530

    refactor: Enable static export in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  // reactStrictMode: true,
+  output: "export", // Add this line to enable static export
 };
+
 export default nextConfig;
